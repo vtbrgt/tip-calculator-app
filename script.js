21d7f3d4dbcb8fb025da83e126712e774691a731
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-05-07 23:13:51 -0300

    several changes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let valorConta = document.querySelector("#bill").value;
-let quantidadeGorjeta = document.querySelector(
-  'input[name="tip_amount"]:checked'
-).value;
+let valorConta = document.querySelector("#bill");
+
+/* parseFloat() */
+
+const button = document.querySelectorAll("botao");
+
+/*
 let gorjetaCustomizada = document.querySelector("#gorjeta_customizada").value;
 let numeroPessoas = document.querySelector("#number_people").value;
 
@@ -9,7 +12,22 @@ let gorjetaPorPessoa = (valorConta * quantidadeGorjeta) / 100 / numeroPessoas;
 
 let totalPorPessoa = valorConta / numeroPessoas + gorjetaPorPessoa;
 
-/* function mostraValor(span) {
+function mostraValor(span) {
   span.textContent = gorjetaPorPessoa;
 }
  */
+
+button.forEach((botao) => {
+  botao.addEventListener("click", selecionarGorjeta);
+});
+
+function selecionarGorjeta(evento) {
+  button.forEach((botao) => {
+    botao.classList.remove("active");
+    if (evento.target.innerHTML === botao.innerHTML) {
+      botao.classList.add("active");
+      quantidadeGorjeta = parseFloat(botao.innerHTML) / 100;
+      console.log(quantidadeGorjeta);
+    }
+  });
+}
