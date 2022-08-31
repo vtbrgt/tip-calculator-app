eae5fb57f868bca5f6c0ddc3b047439c68b68a66
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-30 23:52:30 -0300

    fixing bugs and showing error message

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const button = document.querySelectorAll('.botao');
 const gorjetaCustomizada = document.querySelector('#gorjeta_customizada');
 const numeroPessoas = document.querySelector('#number_people');
 const resultados = document.querySelectorAll('.resultado');
+const erro = document.querySelector('.erro');
 
 let contaValor = 0.0; // valor padrão
 let quantidadeGorjeta = 0.1;
@@ -17,7 +18,7 @@ function setarValorConta() {
   calculaGorjeta();
 }
 
-conta.addEventListener('change', setarValorConta);
+conta.addEventListener('input', setarValorConta);
 
 /* GORJETA */
 function selecionarGorjeta(event) {
@@ -49,7 +50,7 @@ gorjetaCustomizada.addEventListener('click', () => {
     botao.classList.remove('active');
   });
 });
-gorjetaCustomizada.addEventListener('change', valorGorjetaCustomizada);
+gorjetaCustomizada.addEventListener('input', valorGorjetaCustomizada);
 
 /* PESSOAS */
 function numeroDePessoas() {
@@ -58,14 +59,17 @@ function numeroDePessoas() {
   calculaGorjeta();
 }
 
-numeroPessoas.addEventListener('change', numeroDePessoas);
+numeroPessoas.addEventListener('input', numeroDePessoas);
 
 /* CALCULO GORJETA */
 function calculaGorjeta() {
   if (pessoas >= 1) {
+    erro.style.display = 'none';
     let valorGorjeta = (contaValor * quantidadeGorjeta) / pessoas;
     let total = (contaValor * (quantidadeGorjeta + 1)) / pessoas;
     resultados[0].innerHTML = '$' + valorGorjeta.toFixed(2);
     resultados[1].innerHTML = '$' + total.toFixed(2);
+  } else if (pessoas < 1) {
+    erro.style.display = 'block';
   }
 }
